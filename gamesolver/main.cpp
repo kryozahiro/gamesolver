66058bfd89a76bef3e93799472d53a65f77c0d90
kryozahiro <kryozahiro@gmail.com>
2015-11-14 18:15:03 +0900

    switch compilation of main by macro GAMESOLVER_STANDALONE

--- gamesolver/main.cpp
+++ gamesolver/main.cpp
@@ -8,6 +8,8 @@
 #include "Standalone.h"
 using namespace std;
 
+#ifdef GAMESOLVER_STANDALONE
 int main(int argc, char* argv[]) {
 	return Standalone::get_mutable_instance().main(argc, argv);
 }
+#endif
